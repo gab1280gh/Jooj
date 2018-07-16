77ed0c30525cec0d4c109359fbd57dfa0c596b64
gab1280gh <gab1280gh@gmail.com>
2018-07-16 20:17:05 -0300

    inicial

---
+++ Assets/CameraController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+	public float turnSpeed;
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - player.transform.position;
+		player = player;
+	}
+
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void FixedUpdate(){
+		
+	}
+
+	void LateUpdate ()
+	{
+		if (Input.GetButtonDown ("CameraRight")) {
+			offset = Quaternion.AngleAxis (90, Vector3.down) * offset;
+			transform.position = player.transform.position + offset;
+			transform.LookAt (player.transform.position);
+			offset = transform.position - player.transform.position;
+		} 
+
+		transform.position = player.transform.position + offset;
+
+		if (Input.GetButtonDown ("CameraLeft")) {
+			offset = Quaternion.AngleAxis (-90, Vector3.down) * offset;
+			transform.position = player.transform.position + offset;
+			transform.LookAt (player.transform.position);
+			offset = transform.position - player.transform.position;
+		} 
+
+		transform.position = player.transform.position + offset;
+	}
+}
---
+++ Assets/SceneTransition.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame	
+	void Update () {
+
+	}
+
+	void OnTriggerEnter (Collider other){
+		
+		if (other.gameObject.name == "Player") {
+			Debug.Log("collision worked");
+			SceneManager.LoadScene ("interior", LoadSceneMode.Single);
+		}
+
+	}
+
+	IEnumerator LoadYourAsyncScene()
+	{
+		// The Application loads the Scene in the background at the same time as the current Scene.
+		//This is particularly good for creating loading screens. You could also load the Scene by build //number.
+		AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("interior");
+
+		//Wait until the last operation fully loads to return anything
+		while (!asyncLoad.isDone)
+		{
+			yield return null;
+		}
+	}
+}
