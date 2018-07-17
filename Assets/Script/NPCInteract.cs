82ee071b2499054a1ed3ebe45fed92abbcd1f9b8
gab1280gh <gab1280gh@gmail.com>
2018-07-17 19:45:03 -0300

    Balãozinho

---
+++ Assets/Script/NPCInteract.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCInteract : MonoBehaviour {
+
+	public Canvas canvas;
+	public int dialogLevel;
+
+
+	void Start () {
+
+		canvas.enabled = false;
+		dialogLevel = 1;
+
+	}
+
+	void OnTriggerStay (Collider other){
+
+		if (Input.GetButtonDown("Fire1")) {
+
+			if (dialogLevel == 1) {
+				canvas.enabled = true;
+				Debug.Log ("funcionou desgraça");
+				dialogLevel++;
+			}
+			if (dialogLevel == 2) {
+				canvas.enabled = true;
+				Debug.Log ("funcionou denovo");
+				dialogLevel++;
+			}
+			if (dialogLevel == 3) {
+				canvas.enabled = true;
+				Debug.Log ("puta merda funcionou pra caralho");
+				dialogLevel++;
+			}
+			if (dialogLevel >= 4) {
+				canvas.enabled = true;
+				Debug.Log ("agora descobrir como desgraças jogar isso no texto");
+				dialogLevel++;
+			}
+
+		}
+
+	}
+
+	void OnTriggerExit (Collider other){
+	
+		canvas.enabled = false;
+
+	}
+
+	void Update () {
+
+
+	}
+}
---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody cubinho;
+	private GameObject NPCConversa;
+	public float jump;
+	Vector3 forward, right;
+	private bool isFalling = false;
+	float vel = 2f;
+	float turnSpeed = 100f;
+	Quaternion facing;
+	public float fallMultiplier = 2.5f;
+	public float lowJumpMultiplier = 2f;
+
+	// Use this for initialization
+	void Start () {
+
+		cubinho = GetComponent<Rigidbody> ();
+		jump = 30f;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 direction = new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical"));
+		Vector3 rightMove = right * vel * Time.deltaTime * Input.GetAxis ("Horizontal");
+		Vector3 upMove = forward * vel * Time.deltaTime * Input.GetAxis ("Vertical");
+
+
+		Vector3 heading = Vector3.Normalize (rightMove + upMove);
+
+		transform.forward += heading;
+		transform.position += rightMove;
+		transform.position += upMove;
+
+		RaycastHit hit;
+
+		if (Input.GetButtonDown("Jump") && (Physics.Raycast(transform.position, Vector3.down, out hit, 1f))) {
+			cubinho.AddForce(0, jump, 0, ForceMode.Impulse);
+		}
+			
+	}
+
+
+	void FixedUpdate ()
+	{
+		forward = Camera.main.transform.forward;
+		forward.y = 0;
+		forward = Vector3.Normalize (forward);
+		right = Quaternion.Euler (new Vector3 (0, 90, 0)) * forward;	 
+	}
+
+	void Conversar () {
+		
+	}
+		
+}
